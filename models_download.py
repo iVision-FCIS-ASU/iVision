53450716438bde0d1c83bf4c09f951846cceaa92
Devil Assassin <83004869+Devil11Assassin@users.noreply.github.com>
2026-01-25 12:53:00 +0200

    Fully integrated DepthAnythingV2. Added ability to download all models before running.

---
+++ models_download.py
@@ -0,0 +1,83 @@
+import torch
+import requests
+from tqdm import tqdm
+from pathlib import Path
+from ultralytics import YOLO
+from depth_anything_v2.dpt import DepthAnythingV2
+
+def download_file(path: str, url: str):
+    # print(f"Downloading file: {path}")
+    # with requests.get(url, stream=True) as r:
+    #     r.raise_for_status()
+    #     with open(path, "wb") as f:
+    #         for chunk in r.iter_content(chunk_size=1048576):
+    #             f.write(chunk)
+    # print(f"Download done!")
+    print(f"Downloading file: {path}")
+    
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+    chunk_size = 1048576
+    total_size = int(response.headers.get("content-length", 0))
+    
+    with open(path, "wb") as file, tqdm(
+        desc=path, 
+        total=total_size,
+        ncols=100,
+        unit="B", 
+        unit_scale=True, 
+        unit_divisor=1024,
+    ) as bar:
+        for chunk in response.iter_content(chunk_size=chunk_size):
+            if not chunk:
+                continue
+            size = file.write(chunk)
+            bar.update(size)
+    
+    print(f"Download done!")
+
+def download_models():
+    print("\n-----Downloading Models-----\n")
+
+    if not Path("weights/yolo26n.onnx").is_file():
+        model_detect = YOLO("weights/yolo26n.pt", task="detect")
+        model_detect.export(format="onnx")
+    
+    if not Path("weights/yolo26n-seg.onnx").is_file():
+        model_detect = YOLO("weights/yolo26n-seg.pt", task="segment")
+        model_detect.export(format="onnx")
+    
+    if not Path("weights/midas_v21_small_256.pt").is_file():
+        download_file("weights/midas_v21_small_256.pt", "https://github.com/isl-org/MiDaS/releases/download/v2_1/midas_v21_small_256.pt")
+
+    if not Path("weights/dpt_swin2_tiny_256.pt").is_file():
+        download_file("weights/dpt_swin2_tiny_256.pt", "https://github.com/isl-org/MiDaS/releases/download/v3_1/dpt_swin2_tiny_256.pt")
+    
+    if not Path("weights/depth_anything_v2_vits.onnx").is_file():
+        if not Path("weights/depth_anything_v2_vits.pth").is_file():
+            download_file("weights/depth_anything_v2_vits.pth", "https://huggingface.co/depth-anything/Depth-Anything-V2-Small/resolve/main/depth_anything_v2_vits.pth?download=true")
+        else:
+            print("weights/depth_anything_v2_vits.pth already exists!")
+        
+        print("Exporting to ONNX format...")
+        model = DepthAnythingV2(encoder="vits", features=64, out_channels=[48,96,192,384])
+        model.load_state_dict(torch.load("weights/depth_anything_v2_vits.pth", map_location="cpu"))
+        model.eval()
+        dummy = torch.randn(1, 3, 518, 518)
+
+        with torch.no_grad():
+            torch.onnx.export(
+                model, dummy, "weights/depth_anything_v2_vits.onnx",
+                input_names=['input'], output_names=['output'],
+                opset_version=16,
+                do_constant_folding=True,
+                dynamic_axes={
+                    'input': {0: 'batch', 2: 'height', 3: 'width'},
+                    'output': {0: 'batch', 2: 'height', 3: 'width'}
+                },
+                export_params=True,
+                keep_initializers_as_inputs=False,
+            )
+        print("Exported to weights/depth_anything_v2_vits.onnx")
+    
+    print("\n-----All Downloads Completed-----\n")
